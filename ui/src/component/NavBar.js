7638cdd702ec7101f04ca353609281f40b75b8b6
Rafif Elfazri <rafif.elfazri@gmail.com>
2019-08-07 11:48:01 +0700

    Removing Navbar react bootstrap component

--- ui/src/component/NavBar.js
+++ ui/src/component/NavBar.js
@@ -1,30 +1,59 @@
 import React, {Component} from 'react';
 
-import {Navbar, NavDropdown, Nav} from 'react-bootstrap'
-
 
 class NavBar extends Component{
     state = {
         login: "None"
     }
+
+    static setToLog = () => {
+        this.setState({
+            login: ""
+        });
+    }
+
+    checkState = () => {
+        if (this.state.login === "None"){
+            return {display: "none"}   
+        }
+        else{
+            return {display: ""}
+        }
+    }
+        
     render(){
         return (
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">SEA Pay</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="#features">Home</Nav.Link>
-                        <Nav.Link href="#pricing" style={{display:"none"}}>Pricing</Nav.Link>
-                        <NavDropdown title="Rafif" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Edit Profile</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2" style={{display:"none"}}>My Cart</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Logout</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#features">Logout</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">SEA Pay</a>
+                {/* <form className="form-inline">
+                    <div className = "navbar-nav mr-4">
+                        <a className="nav-link " href="#">Home</a>
+                    </div>
+                    <div className = "navbar-nav mr-4">
+                        <a className="nav-link " href="#">Profile</a>
+                    </div>
+                </form> */}
+                <div className="navbar-collapse collapse">
+                    <ul className="navbar-nav pt-1 ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#"></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item dropdown ml-auto">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown link
+                            </a>
+                            <div className="" aria-labelledby="navbarDropdownMenuLink">
+                                <a className="dropdown-item" href="#">Edit Profile</a>
+                                <a className="dropdown-item" href="#">My Cart</a>
+                                <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
