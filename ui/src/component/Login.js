4137c05dc4b3c7516526f3ddb89b165e0f2b089e
Rafif Elfazri <rafif.elfazri@gmail.com>
2019-08-07 11:46:20 +0700

    Changing Login into class

--- ui/src/component/Login.js
+++ ui/src/component/Login.js
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 //import {withRouter} from 'react-router-dom';
 
-const Login = (props) => {
-    const submitClick = (e) => {
-        //e.preventDefault();
-        props.history.push('/customerhome')
+class Login extends Component{
+
+    state = {
+        emailLog: null,
+        passwordLog: null
     }
-    return (
-        <div className = "justify-content-center row">
-        <form onSubmit = {submitClick}>
-            <div className = "text-primary">
-                <h1 className = "display-5 mb-4">Welcome to SEA Pay</h1>
-            </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputEmail1 ">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-            </div>
-            <div className = "text-center">
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </div>     
-        </form>
-    </div>  
-    );    
+
+    handleChange = (e) =>{
+        this.setState({
+            [e.target.id]: e.target.value
+        });     
+    }
+    submitClick = (e) => {
+        console.log(this.state);
+        e.preventDefault();
+        this.props.history.push('/customerhome')
+    }
+    render(){
+        return (
+            <div className = "justify-content-center row">
+            <form onSubmit = {this.submitClick}>
+                <div className = "text-primary">
+                    <h1 className = "display-5 mb-4">Welcome to SEA Pay</h1>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="emailLog">Email address</label>
+                    <input type="email" className="form-control" id="emailLog" aria-describedby="emailHelp" placeholder="Enter email"
+                    onChange = {this.handleChange}/>
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="passwordLog">Password</label>
+                    <input type="password" className="form-control" id="passwordLog" placeholder="Password"
+                    onChange = {this.handleChange}/>
+                </div>
+                <div className = "text-center">
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>     
+            </form>
+        </div>  
+        );
+    }    
 }
 export default Login;
