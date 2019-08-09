3492ac113772c89768c515e36934eceaad537f44
Rafif Elfazri <rafif.elfazri@gmail.com>
2019-08-09 15:48:59 +0700

    [rafif] adding panel for all merchant item

---
+++ ui/src/component/MerchantPage/ItemList.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class ItemList extends Component{
+
+    render(){
+        return(
+            <div className = "container border border-width" style = {{width:300, height: 300, overflow: "auto"}}>
+                
+            </div>
+        );
+    }
+}
