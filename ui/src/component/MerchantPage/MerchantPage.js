ac323968cb2e0f97e742b79dea0cbac1903f254a
Rafif Elfazri <rafif.elfazri@gmail.com>
2019-08-09 15:49:53 +0700

    [rafif] Adding Merchant Profile Panel

--- ui/src/component/MerchantPage/MerchantPage.js
+++ ui/src/component/MerchantPage/MerchantPage.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
 
 import AddItemForm from './AddItemForm.js'
+import ItemList from "./ItemList"
+import MerchantProfile from "./MerchantProfie"
 
 export default class MerchantPage extends Component{
     render(){
         return(
-            <div className="container">
+            <div className="container mt-2">
                 <div className="row justify-content">
-                    <AddItemForm/>
+                    <div className="col text-center mb-4">
+                        <h4>Information</h4>
+                        <MerchantProfile/>
+                    </div>
+                    <div className="col text-center mb-4">
+                        <h4>Add Item</h4>
+                        <AddItemForm/>
+                    </div>
+                    <div className="col text-center mb-4">
+                        <h4>Item List</h4>
+                        <ItemList/>
+                    </div>
+
                 </div>
             </div>
         );
---
+++ ui/src/component/MerchantPage/MerchantProfie.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+
+export default class MerchantProfile extends Component{
+
+    render(){
+        return(
+            <div className="col text-center mb-4">
+                <div className = "container border border-width" style = {{width: 300, height: 300 }}>
+                </div>
+            </div>
+        );
+            
+        
+    }
+}
