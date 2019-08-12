436e414139ac3995914bc3694c32a4fe4a966599
Rafif Elfazri <rafif.elfazri@gmail.com>
2019-08-12 09:38:27 +0700

    [rafif] Adding ProposalList Panel

---
+++ ui/src/component/AdminPage/ProposalList.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+
+export default class ProposalList extends Component{
+
+    render(){
+        return(
+            <div className = "container border border-width" style = {{width:300, height: 300, overflow: "auto"}}>
+                
+                
+            </div>
+
+        );
+    }
+}
