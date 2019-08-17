4166c39e5d5ff91e8e914eb9800cba4f57050407
aeramu <sulam3010@gmail.com>
2019-08-17 09:00:49 +0700

    [alam] add WalletService.java

--- app/module/Wallet.java
+++ app/module/Wallet.java
@@ -7,15 +7,37 @@ public class Wallet {
     private static ArrayList<Wallet> listOfWallet = new ArrayList<Wallet>();
     private static int numOfWallet = 0;
     private long id;
-    private double money;
+    private long balance;
     private double seaPoint;
+    private Long userId;
 
-    public Wallet(){
+    public Wallet(Long userId){
         numOfWallet += 1;
         this.id = numOfWallet;
-        this.money = 0;
+        this.balance = 0;
         this.seaPoint = 0;
+        this.userId = userId;
         listOfWallet.add(this);
+    }
+
+    public void addBalance(long amount){
+        this.balance += amount;
+    }
+
+    public void substractBalance(Long balance) {
+        this.balance =- balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserID() {
+        return userId;
+    }
+
+    public Long getBalance() {
+        return this.balance;
     }
 
 }
